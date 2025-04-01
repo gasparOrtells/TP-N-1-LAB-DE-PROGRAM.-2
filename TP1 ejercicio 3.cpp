5c05ac599cdb5e329d0ceee6befbbd03ff0b9d21
gasparOrtells <gasparortells27@gmail.com>
2025-04-01 11:10:46 -0300

    Add files via upload

---
+++ TP1 ejercicio 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1;
+	int num2; 
+	int num3;
+	cout<<"ingrese el primer numero"<<endl;
+	cin>>num1;
+	cout<<"ingrese el segundo numero"<<endl;
+	cin>>num2;
+	cout<<"ingrese el tercer numero"<<endl;
+	cin>>num3;
+	if(num1<10 && num2<10 && num3<10) {
+		cout<<"todos los numeros son menores a 10"<<endl;
+return 0;
+}
+
+}
